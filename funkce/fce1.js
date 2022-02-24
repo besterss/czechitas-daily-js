7971d74b3ed1954c0c38ed43724a2bfc225cf62c
besterss <prochazka@unodor.cz>
2022-02-24 18:54:04 +0100

    1 file modified

--- funkce/fce1.js
+++ funkce/fce1.js
@@ -1,6 +1,17 @@
-/*
 
+
+/* Úkol č.1:
 Napište funkci, která vypíše součet, rozdíl, násobek a podíl 2
 zadaných čísel, rozdíl zobrazujte jako absolutní hodnotu.
+*/
 
+
+
+/* Úkol č.2:
+Napište funkci, který vypíše informaci, zda zadané číslo je
+nebo není prvočíslo.
+- prvočíslo - dělitelné pouze 1 a samo sebou
+- zbytek po dělení: % - modulo
+- v první moment pčedpokládejme, ze číslo JE prvočíslo a
+budeme případně rozporovat toto tvrzeni.
 */
