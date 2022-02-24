905a647bfa7d0dfc7f0d51011faf2f908777652e
besterss <prochazka@unodor.cz>
2022-02-24 19:03:38 +0100

    1 file modified

--- funkce/fce1.js
+++ funkce/fce1.js
@@ -5,6 +5,22 @@ Napište funkci, která vypíše součet, rozdíl, násobek a podíl 2
 zadaných čísel, rozdíl zobrazujte jako absolutní hodnotu.
 */
 
+function srnp(x,y) {
+ let soucet = x+y;
+ let rozdil = x-y;
+ let soucin = x*y;
+ let podil = x/y;
+
+ console.log("Soucet: "+x+"+"+y+"="+soucet)
+ console.log("Rozdíl: "+x+"-"+y+"="+rozdil)
+ console.log("Součin: "+x+"*"+y+"="+soucin)
+ console.log("Podíl: "+x+"/"+y+"="+podil)
+}
+
+let x = Number(prompt("Zadej X"))
+let y = Number(prompt("Zadej Y"))
+
+srnp(x,y)
 
 
 /* Úkol č.2:
